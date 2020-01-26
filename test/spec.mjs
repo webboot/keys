5d8a6297e00a84c11de4c504a0b33c63f3e2d5cc
jascha <jascha@jaeh.at>
2020-01-26 06:24:47 +0100

    add tests

---
+++ test/spec.mjs
@@ -0,0 +1,16 @@
+import path from 'path'
+import fs from 'fs'
+
+import { is } from '@magic/test'
+
+import key, { fingerprint, armored } from '../src/index.mjs'
+
+const keyPath = path.join(process.cwd(), 'src', 'webboot.asc')
+
+const pubKeyFileString = fs.readFileSync(keyPath, 'utf8')
+
+export default [
+  { fn: pubKeyFileString, expect: is.len.lt(10), info: 'webboot.asc is not empty' },
+  { fn: pubKeyFileString.trim(), expect: armored.trim(), info: 'webboot.asc equals armored js export' },
+  { fn: key.fingerprint, expect: fingerprint, info: 'key.fingerprint and export fingerprint match' },
+]
